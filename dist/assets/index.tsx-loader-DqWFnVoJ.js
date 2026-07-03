6df81be60c7db7047b1d59577dd39a8c85c0dc67
shree <shreerajgudade124@gmail.com>
2026-07-03 21:23:22 +0530

    fix of all

--- dist/assets/index.tsx-loader-DqWFnVoJ.js
+++ dist/assets/index.tsx-loader-DqWFnVoJ.js
@@ -5,7 +5,7 @@
   (async () => {
     const { onExecute } = await import(
       /* @vite-ignore */
-      chrome.runtime.getURL("assets/index.tsx-TJAUAhE-.js")
+      chrome.runtime.getURL("assets/index.tsx-CDgghA-2.js")
     );
     onExecute?.({ perf: { injectTime, loadTime: performance.now() - injectTime } });
   })().catch(console.error);
