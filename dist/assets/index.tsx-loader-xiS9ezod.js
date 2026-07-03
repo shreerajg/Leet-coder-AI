c819ada5ddc6068ffe816156709dd7d2b8bd827a
shree <shreerajgudade124@gmail.com>
2026-07-03 21:26:52 +0530

    fix of all

--- dist/assets/index.tsx-loader-xiS9ezod.js
+++ dist/assets/index.tsx-loader-xiS9ezod.js
@@ -5,7 +5,7 @@
   (async () => {
     const { onExecute } = await import(
       /* @vite-ignore */
-      chrome.runtime.getURL("assets/index.tsx-CDgghA-2.js")
+      chrome.runtime.getURL("assets/index.tsx--1NZ_AOG.js")
     );
     onExecute?.({ perf: { injectTime, loadTime: performance.now() - injectTime } });
   })().catch(console.error);
